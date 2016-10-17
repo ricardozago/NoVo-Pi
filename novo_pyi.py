377ec8b8cbf6ebc96ffb04608a185770b90683bd
Arthur Zanatta da Costa <arthurzc23@gmail.com>
2016-10-17 20:57:50 +0000

    Create novo_pyi.py

---
+++ novo_pyi.py
@@ -0,0 +1,100 @@
+import subprocess
+import argparse
+
+
+def get_args():
+
+    #Parse the command line into 'True/False' values.
+    parser = argparse.ArgumentParser(description="Coordinate NoVo Pi")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-a', '--activate', action='store_true', help = 'Start NoVo Pi', required=False)
+    group.add_argument('-s', '--stop', action='store_true', help = 'Stop NoVo Pi', required=False)
+    group.add_argument('-r', '--restart', action='store_true', help = 'Restart NoVo Pi', required=False)
+    
+    #args it is a namespace
+    args = parser.parse_args()
+    return args
+    
+def main():
+
+    args = get_args()
+         
+        if args.activate:
+
+
+          #Boot LED pattern
+	
+
+
+        active_flag = False
+
+        #Check if module is already activated
+
+        proc_1 = subprocess.Popen(['pgrep', '-f', 'mopidy'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        out, err = proc_1.communicate()
+
+        proc_1.wait()
+        
+
+        if out != '':
+
+            active_flag = True
+        
+
+        #Activate module
+
+        if active_flag == False:
+
+            proc_2 = subprocess.call(['lxterminal', '-e','mopidy'])
+
+            
+
+    
+        
+        
+
+    elif args.stop:
+
+
+        active_flag = False
+
+        #Check if module is already activated
+
+        proc_1 = subprocess.Popen(['pgrep', '-f', 'mopidy'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        out, err = proc_1.communicate()
+
+        proc_1.wait()
+        
+
+        if out != '':
+
+            active_flag = True
+        
+        #Stop module
+
+        if active_flag == True:
+
+
+            #proc_2 = subprocess.Popen(['lxterminal', '-e', 'pgrep', '-f', 'mopidy', 'awk', '\'{print \"kill -9 \" $1}\'', 'sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            #proc_2 = subprocess.call('/home/pi/Desktop/mic/novo_pi/bash_script.sh', shell=True)
+
+            proc_2 = subprocess.call('./bash_script.sh', shell=True)
+                                             
+
+            
+            
+
+        
+
+    elif args.restart:
+
+        _ = 1
+
+
+    #Check if mopidy is active. If it is, restart. Else, start.
+
+
+main()
